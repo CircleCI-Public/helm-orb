6daaf14d154367bf201613f4dff440982af2b239
Jeff Chen <jeff.chen@circleci.com>
2022-05-02 11:18:53 -0700

    fix release-name

--- src/scripts/upgrade-helm-chart.sh
+++ src/scripts/upgrade-helm-chart.sh
@@ -16,27 +16,6 @@ fi
 if [ "${ORB_PARAM_WAIT}" == "true" ]; then
   set -- "$@" --wait
 fi
-if [ "${TLS}" == "true" ]; then
-  set -- "$@" --tls
-fi
-if [ -n "${TLS_CA_CERT}" ]; then
-  set -- "$@" --tls-ca-cert="${TLS_CA_CERT}"
-fi
-if [ -n "${TLS_CERT}" ]; then
-  set -- "$@" --tls-cert="${TLS_CERT}"
-fi
-if [ -n "${TLS_HOSTNAME}" ]; then
-  set -- "$@" --tls-hostname="${TLS_HOSTNAME}"
-fi
-if [ -n "${TLS_KEY}" ]; then
-  set -- "$@" --tls-key="${TLS_KEY}"
-fi
-if [ "${TLS_VERIFY}" == "true" ]; then
-  set -- "$@" --tls-verify
-fi
-if [ -n "${TILLER_NAMESPACE}" ]; then
-  set -- "$@" --tiller-namespace "${TILLER_NAMESPACE}"
-fi
 if [ -n "${DEVEL}" ]; then
   set -- "$@" --devel "${DEVEL}"
 fi
