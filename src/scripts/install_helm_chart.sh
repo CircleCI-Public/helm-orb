7c8d0bc294078e34580bf42c22183d2d290ab64f
Brian Vu <emailbrian.vu@gmail.com>
2023-09-05 16:29:38 -0700

    chore: remove quotes from boolean values

--- src/scripts/install_helm_chart.sh
+++ src/scripts/install_helm_chart.sh
@@ -8,13 +8,13 @@ set -x
 if [ -n "${HELM_STR_VALUES_TO_OVERRIDE}" ]; then
   set -- "$@" --set "${HELM_STR_VALUES_TO_OVERRIDE}"
 fi
-if [ "${HELM_BOOL_WAIT_FOR_JOBS}" -eq "1" ]; then
+if [ "${HELM_BOOL_WAIT_FOR_JOBS}" -eq 1 ]; then
   set -- "$@" --wait-for-jobs
 fi
 if [ -n "${HELM_STR_NAMESPACE}" ]; then
   set -- "$@" --namespace="${HELM_STR_NAMESPACE}"
 fi
-if [ "${HELM_BOOL_WAIT}" -eq "1" ]; then
+if [ "${HELM_BOOL_WAIT}" -eq 1 ]; then
   set -- "$@" --wait
 fi
 if [ -n "${HELM_STR_RELEASE_NAME}" ]; then
--- src/scripts/upgrade_helm_chart.sh
+++ src/scripts/upgrade_helm_chart.sh
@@ -19,25 +19,25 @@ fi
 if [ "${HELM_BOOL_NO_HOOKS}" = "1" ]; then
   set -- "$@" --no-hooks
 fi
-if [ "${HELM_BOOL_RECREATE_PODS}"  -eq "1" ]; then
+if [ "${HELM_BOOL_RECREATE_PODS}"  -eq 1 ]; then
   set -- "$@" --recreate-pods
 fi
-if [ "${HELM_BOOL_ATOMIC}" -eq "1" ]; then
+if [ "${HELM_BOOL_ATOMIC}" -eq 1 ]; then
   set -- "$@" --atomic
 fi
-if [ "${HELM_BOOL_WAIT}" -eq "1" ]; then
+if [ "${HELM_BOOL_WAIT}" -eq 1 ]; then
   set -- "$@" --wait
 fi
 if [ -n "${HELM_STR_DEVEL}" ]; then
   set -- "$@" --devel "${HELM_STR_DEVEL}"
 fi
-if [ "${HELM_BOOL_DRY_RUN}" -eq "1" ]; then
+if [ "${HELM_BOOL_DRY_RUN}" -eq 1 ]; then
   set -- "$@" --dry-run
 fi
-if [ "${HELM_BOOL_RESET_VALUES}" -eq "1" ]; then
+if [ "${HELM_BOOL_RESET_VALUES}" -eq 1 ]; then
   set -- "$@" --reset-values
 fi
-if [ "${HELM_BOOL_REUSE_VALUES}" -eq "1" ]; then
+if [ "${HELM_BOOL_REUSE_VALUES}" -eq 1 ]; then
   set -- "$@" --reuse-values
 fi
 if [ -n "${HELM_STR_VALUES}" ]; then
@@ -49,10 +49,10 @@ fi
 if [ -n "${HELM_STR_VERSION}" ]; then
   set -- "$@" --version="${HELM_STR_VERSION}"
 fi
-if [ "${HELM_BOOL_WAIT_FOR_JOBS}" -eq "1" ]; then
+if [ "${HELM_BOOL_WAIT_FOR_JOBS}" -eq 1 ]; then
   set -- "$@" --wait-for-jobs
 fi
-if [ "${HELM_BOOL_FORCE}" -eq "1" ]; then
+if [ "${HELM_BOOL_FORCE}" -eq 1 ]; then
   set -- "$@" --force
 fi
 
