197bc6c340e776b911ef0b923f451cccea9f955a
Mateo Arboleda <mateo.arboleda@circleci.com>
2024-10-21 11:19:26 -0500

    Add retry logic

--- src/scripts/install_helm_client.sh
+++ src/scripts/install_helm_client.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 HELM_STR_VERSION="$(echo "${HELM_STR_VERSION}" | circleci env subst)"
+HELM_RETRIES="$(echo "${HELM_RETRIES}" | circleci env subst)"
 
 set -x
 if command -v helm &> /dev/null; then
@@ -13,7 +14,24 @@ fi
 
 INSTALL_SCRIPT="https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3"
 
-curl "${INSTALL_SCRIPT}" > get_helm.sh
+success_helm_download=false
+retry_count=0
+while [ "$retry_count" -lt "$HELM_RETRIES" ]; do
+  curl -sSf "${INSTALL_SCRIPT}" -o get_helm.sh
+  if [ $? -eq 0 ]; then
+    echo "Script downloaded succesfully"
+    success_helm_download=true
+    break
+  else
+    echo "Error downloading. Retying..."
+    retry_count=$((rety_count + 1))
+    sleep 5
+  fi
+done
+if [ "$success_helm_download" = false ]; then
+  echo "Max retries reached"
+  exit 1
+fi
 chmod 700 get_helm.sh
 ./get_helm.sh "$@"
 
