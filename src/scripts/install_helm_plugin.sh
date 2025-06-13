70513769cacd7001d5020ae5e6efcfd9e37ddaf2
marboledacci <mateo.arboleda@circleci.com>
2025-06-13 12:54:32 -0500

    Add plugin version parameter (#87)
    * Add plugin version parameter
    
    * update syntax

--- src/scripts/install_helm_plugin.sh
+++ src/scripts/install_helm_plugin.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 HELM_STR_PLUGIN_URL="$(echo "${HELM_STR_PLUGIN_URL}" | circleci env subst)"
+HELM_STR_PLUGIN_VERSION="$(echo "${HELM_STR_PLUGIN_VERSION}" | circleci env subst)"
 
-STATUS="$(helm plugin install "${HELM_STR_PLUGIN_URL}")"
+if [ -n "$HELM_STR_PLUGIN_VERSION" ]; then
+  STATUS="$(helm plugin install "${HELM_STR_PLUGIN_URL}" --version "${HELM_STR_PLUGIN_VERSION}")"
+else
+  STATUS="$(helm plugin install "${HELM_STR_PLUGIN_URL}")"
+fi
 
 if echo "${STATUS}" | grep "Installed plugin:"; then
   echo "Installation successful"
